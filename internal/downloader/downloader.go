217cc60a3eca02a38e38db91d2331fe32da801b3
Your Name <pavlikkim200401@gmail.com>
2023-02-09 20:39:10 +0600

    updated info in terminal

--- internal/downloader/downloader.go
+++ internal/downloader/downloader.go
@@ -1,7 +1,9 @@
 package download
 
 import (
+	"fmt"
 	"go-wget/internal/service"
+	utils "go-wget/pkg"
 	"net/http"
 	"regexp"
 	"strconv"
@@ -26,6 +28,10 @@ func NewDownloader(cl *http.Client, service *service.Service, re *regexp.Regexp)
 
 func (d *Downloader) Download(url, path string) error {
 	filename := d.filenameRegexp.FindStringSubmatch(url)
+
+	fmt.Printf("starts at %v\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("sending request, awaiting response... ")
+
 	resp, err := d.httpClient.Get(url)
 	if resp == nil {
 		return ErrResponceNil
@@ -35,12 +41,18 @@ func (d *Downloader) Download(url, path string) error {
 	}
 	defer resp.Body.Close()
 
+	fmt.Println(resp.Status)
+
 	size, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
+
+	fmt.Printf("content size: %d [~%fMB]\n", size, utils.ToMB(size))
+	fmt.Printf("saving file to: %s\n", path+filename[1])
 
 	bar := pb.New(int(size)).SetRefreshRate(time.Second).SetUnits(pb.U_BYTES)
 	bar.ShowSpeed = true
 	bar.ShowTimeLeft = true
 	bar.Prefix(filename[1] + "         ")
+
 	bar.Start()
 	reader := bar.NewProxyReader(resp.Body)
 	if err := d.service.GetFileWithContentLength(filename[1], path, reader); err != nil {
@@ -48,5 +60,7 @@ func (d *Downloader) Download(url, path string) error {
 	}
 	bar.Finish()
 
+	fmt.Printf("\nDownloaded [%s]\n", url)
+	fmt.Printf("finished at %v\n", time.Now().Format("2006-01-02 15:04:05"))
 	return nil
 }
---
+++ pkg/musor.go
@@ -0,0 +1,5 @@
+package utils
+
+func ToMB(value int) float64 {
+	return float64(value) / (1024 * 1024)
+}
